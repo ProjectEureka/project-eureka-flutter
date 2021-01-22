b30c99bfa1a44ef00bb2a4b3f13517667c881329
dominikyz <64664704+dominikyz@users.noreply.github.com>
2021-01-22 13:13:00 -0800

    PE-19 | First Time Onboarding Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:project_eureka_flutter/screens/profile_onboarding.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'onboarding.dart';
 
-void main() => runApp(MyApp());
+//first time login checker
+int initScreen;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  initScreen = await prefs.getInt("initScreen");
+  await prefs.setInt("initScreen", 1); //after first launch set to 1
+  print(
+      'initScreen ${initScreen}'); // printout to see how many times app was launched
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,8 +25,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: ProfileOnboarding(),
+      ), // check if it's the first launch of app
+      initialRoute: initScreen == 0 || initScreen == null ? "first" : "/",
+      routes: {
+        '/': (context) => placeholder(),
+        "first": (context) => onboarding(),
+      },
       debugShowCheckedModeBanner: false,
     );
   }
