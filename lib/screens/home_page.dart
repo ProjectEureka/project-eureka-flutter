d571c941487067fb0d6a6d96343a2c7dc37f587f
Max <maxbogat717@gmail.com>
2021-03-20 21:01:30 -0700

    PE-61 | Fixed bug with refreshing while searching or with selected category

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -35,6 +35,8 @@ class _HomeState extends State<Home> {
 
   Future<void> _getData() async {
     setState(() {
+      selectedCategory = "All Categories";
+      isSearching = false;
       initGetQuestions();
     });
   }
