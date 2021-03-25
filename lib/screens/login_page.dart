0ff509bbd49481c0ed49b24ef77ff04beb3101a4
Tony <tonynguyen0925@gmail.com>
2021-03-25 14:38:12 -0700

    PE-64 | Fixed missing await

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -72,7 +72,7 @@ class _LoginPageState extends State<LoginPage> {
     _formKey.currentState.save();
 
     try {
-      _emailAuth.signIn(email, password);
+      await _emailAuth.signIn(email, password);
 
       Navigator.pushAndRemoveUntil(
         context,
