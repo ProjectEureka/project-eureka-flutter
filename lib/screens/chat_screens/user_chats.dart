26b678bb3ebfd40cfc403596d8f7b7d09d6a0201
Viktor Tatarenko <viktor.tatarenko.240@my.csun.edu>
2021-04-29 21:37:23 -0700

    Updated colors and adjusted text

--- lib/screens/chat_screens/user_chats.dart
+++ lib/screens/chat_screens/user_chats.dart
@@ -160,7 +160,7 @@ class ConversationBubble extends StatelessWidget {
                           ],
                         ),
                       )
-                    ],
+                    ],:
                   ),
                 ),
               ),
